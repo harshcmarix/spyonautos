626e8311b5a90e4f0d9a294e420974f67304612a
Harsh <harshm.cmarix@gmail.com>
2021-06-09 17:14:58 +0530

    Add scrape date only once when import

--- app/Console/Commands/RenaultImport.php
+++ app/Console/Commands/RenaultImport.php
@@ -108,7 +108,10 @@ class RenaultImport extends Command
                     $productModel->review_score = !empty($productDetail['review_score']) ? $productDetail['review_score'] : '';
                     $productModel->review_count = !empty($productDetail['review_count']) ? $productDetail['review_count'] : 0;
                     $productModel->url = !empty($productDetail['url']) ? $productDetail['url'] : '';
-                    $productModel->scrape_date = !empty($productDetail['scrape_date']) ? $productDetail['scrape_date'] : '';
+                    // Add scrape date only once
+                    if ($isProductInsert == true) {
+                        $productModel->scrape_date = !empty($productDetail['scrape_date']) ? $productDetail['scrape_date'] : null;
+                    }
                     $productModel->listing_id = !empty($productDetail['listing_id']) ? $productDetail['listing_id'] : '';
                     $productModel->listing_date = !empty($productDetail['listing_date']) ? $productDetail['listing_date'] : null;
                     $productModel->car_id = !empty($productDetail['car_id']) ? $productDetail['car_id'] : 0;
